1ef4bdcdef96da58a054d7e4b8d5d1e48f112d5d
Güven TUNCAY <50626234+guventuncay@users.noreply.github.com>
2020-12-25 08:45:04 +0300

    mvnw packge fix

--- src/test/java/com/guvenx/springboot/thymleafdemo/ThymleafdemoApplicationTests.java
+++ src/test/java/com/guvenx/springboot/thymleafdemo/ThymleafdemoApplicationTests.java
@@ -1,9 +1,11 @@
 package com.guvenx.springboot.thymleafdemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootConfiguration
 class ThymleafdemoApplicationTests {
 
 	@Test
